70666c81f47336c298d264cd7c16cf198a5dd3ae
Milor <yuwenbo00@163.com>
2022-08-05 17:16:19 +0800

    Create AnonymousInnerClassThread.java

---
+++ Snail/src/main/java/org/ilearning/thread/AnonymousInnerClassThread.java
@@ -0,0 +1,25 @@
+
+package org.ilearning.thread;
+
+/**
+ * 创建线程方式：匿名内部类实现线程
+ *
+ * @author yWX983890
+ * @since 2022-08-05
+ */
+class AnonymousInnerClassThread {
+
+    public void Anonymous() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("子线程开始启动......");
+                for (int i = 0; i <= 30; i++) {
+                    System.out.println("run i:" + i);
+                }
+            }
+
+        });
+        thread.start();
+    }
+}

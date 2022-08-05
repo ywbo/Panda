e7c8c59a84f5e016acfbf8b2669a3aaa9eb63919
Milor <yuwenbo00@163.com>
2022-08-05 17:12:56 +0800

    Create SnailApplication.java

---
+++ Snail/src/main/java/org/ilearning/SnailApplication.java
@@ -0,0 +1,15 @@
+package org.ilearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class SnailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnailApplication.class, args);
+        System.out.println("Hello World!");
+    }
+}

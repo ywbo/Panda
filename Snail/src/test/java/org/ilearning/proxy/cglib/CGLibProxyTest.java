28c9b07d93f84941a23a0f64efea08cfdce7d3b0
yuwenbo <yuwenbo00@163.com>
2022-08-06 23:26:22 +0800

    [DESCRIPTION]: [proxy] sort out jdk proxy test and cglib proxy test. [MODULE]: Snail [BRANCH]: master

---
+++ Snail/src/test/java/org/ilearning/proxy/cglib/CGLibProxyTest.java
@@ -0,0 +1,10 @@
+package org.ilearning.proxy.cglib;
+
+/**
+ * CGLib测试类
+ *
+ * @author yuwenbo
+ * @date 2022/8/6 23:19
+ **/
+public class CGLibProxyTest {
+}
--- Snail/src/test/java/org/ilearning/proxy/jdk/JDKProxyTest.java
+++ Snail/src/test/java/org/ilearning/proxy/jdk/JDKProxyTest.java
@@ -1,9 +1,9 @@
-package org.ilearning.proxy;
+package org.ilearning.proxy.jdk;
 
-import org.ilearning.proxy.dao.IHaloProxy;
-import org.ilearning.proxy.dynamic.DynamicProxy;
-import org.ilearning.proxy.dynamic.HaloProxy;
-import org.ilearning.proxy.statical.StaticProxy;
+import org.ilearning.proxy.jdk.dao.IHaloProxy;
+import org.ilearning.proxy.jdk.dynamic.DynamicProxy;
+import org.ilearning.proxy.jdk.dynamic.HaloProxy;
+import org.ilearning.proxy.jdk.statical.StaticProxy;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,8 +21,8 @@ import java.lang.reflect.Proxy;
  * @author yuwenbo
  * @date 2022/8/6 22:23
  **/
-public class ProxyTest {
-    private Logger logger = LoggerFactory.getLogger(ProxyTest.class);
+public class JDKProxyTest {
+    private Logger logger = LoggerFactory.getLogger(JDKProxyTest.class);
 
     /**
      * 静态代理测试类

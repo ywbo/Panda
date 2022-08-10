7456eb1fa8efb77e60af681df5e6f7c3f8d0601d
yuwenbo <yuwenbo00@163.com>
2022-08-11 00:31:00 +0800

    [DESCRIPTION]: [proxy] JMockit unit test class. [MODULE]: Snail [BRANCH]: master

--- Snail/src/main/java/org/ilearning/jmockit/Calculator.java
+++ Snail/src/main/java/org/ilearning/jmockit/Calculator.java
@@ -1,5 +1,8 @@
 package org.ilearning.jmockit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * JMockit 单元测试框架
  *
@@ -7,6 +10,11 @@ package org.ilearning.jmockit;
  * @date 2022/8/10 23:09
  **/
 public class Calculator {
+    /**
+     * 日志
+     */
+    private Logger logger = LoggerFactory.getLogger(Calculator.class);
+
     /**
      * 非静态函数
      */
@@ -26,5 +34,26 @@ public class Calculator {
      */
     public void noop() {
         System.out.println("没有被Mock");
+    }
+
+    /**
+     * 私有函数
+     */
+    private void privateMethod() {
+        System.out.println("private method");
+    }
+
+    /**
+     * 无参构造函数初始化
+     */
+    public Calculator() {
+        System.out.println("无参构造函数初始化");
+    }
+
+    /**
+     * 静态代码块初始化
+     */
+    static {
+        System.out.println("静态代码块初始化");
     }
 }
---
+++ Snail/src/main/java/org/ilearning/jmockit/JMockit使用.md
@@ -0,0 +1,1 @@
+[JMockit使用](https://www.letianbiji.com/java-jmockit/jmockit-MockUp.html)

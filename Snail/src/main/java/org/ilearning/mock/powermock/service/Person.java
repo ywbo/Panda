42a6bdc6ceab2d62001938aeb0fea48388fbbbca
Milor <yuwenbo00@163.com>
2022-08-15 17:19:58 +0800

    Create Person.java

---
+++ Snail/src/main/java/org/ilearning/mock/powermock/service/Person.java
@@ -0,0 +1,17 @@
+
+package org.ilearning.mock.powermock.service;
+
+/**
+ * 功能描述
+ *
+ * @author yuwenbo
+ * @since 2022-08-15
+ */
+public interface Person {
+    /**
+     * 获取姓名
+     *
+     * @return String
+     */
+    String getName();
+}

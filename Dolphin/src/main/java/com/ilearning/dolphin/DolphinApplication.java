62464173a967ebd56edc81cf56cbe8248ee9232b
yuwenbo <yuwenbo00@163.com>
2022-11-07 22:05:54 +0800

    [DESCRIPTION]: [add] Dolphin模块，用于测试框架结构代码 [MODULE]: Dolphin [BRANCH]: master

---
+++ Dolphin/src/main/java/com/ilearning/dolphin/DolphinApplication.java
@@ -0,0 +1,13 @@
+package com.ilearning.dolphin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphinApplication.class, args);
+    }
+
+}
---
+++ Dolphin/src/test/java/com/ilearning/dolphin/DolphinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilearning.dolphin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolphinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

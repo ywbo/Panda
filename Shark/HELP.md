763b789359d8e0736512de900c572935639d5d68
yuwenbo <yuwenbo00@163.com>
2023-02-13 22:52:48 +0800

    [DESCRIPTION]: [算法刷题] [MODULE]: shark [BRANCH]: master

---
+++ Shark/HELP.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+### 鲨鱼鲸鱼计算-鲸算（Shark Algorithm）
+
+* 本项目应用于算法（Algorithm）
---
+++ Shark/src/main/java/com/ilearning/shark/SharkApplication.java
@@ -0,0 +1,13 @@
+package com.ilearning.shark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharkApplication.class, args);
+    }
+
+}
